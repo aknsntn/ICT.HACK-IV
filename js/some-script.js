cfd06d879945286716356cdaf6b2596f5424499e
timsmr <smrtim17@gmail.com>
2022-05-22 14:24:09 +0300

    fixed some extention problems

--- js/some-script.js
+++ js/some-script.js
@@ -1,39 +1,40 @@
-chrome.storage.sync.get({value: false}, (data) => {
-    if (data.value) {
-        console.log('12345')
-        // adding scripts to the page 
-        const SCRIPT_NAMES = ['opencv.js', 'face-api.min.js']
 
-        for (let i=0; i < SCRIPT_NAMES.length; i++) {
-            let script = document.createElement('script');
-            script.defer = true;
-            script.src = chrome.runtime.getURL(`'js/${SCRIPT_NAMES[i]}'`);
-            document.head.prepend(script)
+async function loadModels() {
+    const url = chrome.runtime.getURL('../models')
+    Promise.all([
+        await faceapi.nets.tinyFaceDetector.loadFromUri('../models'),
+        // await faceapi.nets.faceRecognitionNet.loadFromUri('../models'),
+        await faceapi.nets.faceExpressionNet.loadFromUri('../models')
+    ])
+}
+
+
+async function main() {
+    chrome.storage.sync.get({value: false}, (data) => {
+        if (data.value) { 
+            
+            const videoList = document.getElementsByTagName('video')
+            if (videoList.length) {
+                const video = videoList[0]
+                loadModels();
+
+                video.addEventListener('playing', () => {
+                    const canvas = faceapi.createCanvasFromMedia(video)
+                    canvas.style.position = "absolute"
+                    video.after(canvas)
+                    const displaySize = { width: video.width, height: video.height }
+                    faceapi.matchDimensions(canvas, displaySize)
+                    setInterval(async () => {
+                        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceExpressions()
+                        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+                        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+                        faceapi.draw.drawDetections(canvas, resizedDetections)
+                        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+                    }, 100)
+                })
+            }
         }
+    })
+}
 
-        const video = document.getElementsByTagName('video')
-        console.log(video)
-        // Promise.all([
-        //     faceapi.nets.tinyFaceDetector.loadFromUri('../models'),
-        //     faceapi.nets.faceLandmark68Net.loadFromUri('../models'),
-        //     faceapi.nets.faceRecognitionNet.loadFromUri('../models'),
-        //     faceapi.nets.faceExpressionNet.loadFromUri('../models')
-        // ])
-        console.log(video)
-        video.addEventListener('playing', () => {
-            console.log('--------------------------------')
-            const canvas = faceapi.createCanvasFromMedia(video)
-            canvas.classList.add('rec-canvas')
-            video.after(canvas)
-            const displaySize = { width: video.width, height: video.height }
-            faceapi.matchDimensions(canvas, displaySize)
-            setInterval(async () => {
-                const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-                const resizedDetections = faceapi.resizeResults(detections, displaySize)
-                canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-                faceapi.draw.drawDetections(canvas, resizedDetections)
-                faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-            }, 100)
-        })
-    }
-})
+main()
